9bc6e9ef9e29cfd18cae4b20f3acd55b6a2ce3d6
LMaverick <91562063+LMaverick@users.noreply.github.com>
2021-12-27 13:12:45 -0300

    Update stage1.js

--- Jogos/Labirinto-game/js/stage1.js
+++ Jogos/Labirinto-game/js/stage1.js
@@ -1,6 +1,39 @@
 var stage1State = {
     create:function(){
-        alert();
+        //adicionar imagem de fundo
+        game.add.sprite(0,0,'bg');
+
+        //matrix do labirinto
+        this.maze = [// 1 = bloco, 0 = caminho, 2 = player spawn, 3 =moedas spawn
+            //ficam dentro de uma submatrix
+            [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],//15 column 10 row
+            [1,0,0,0,1,0,0,0,0,0,0,0,0,0,1],
+            [1,0,1,1,0,0,1,0,1,1,1,0,1,0,1],
+            [1,0,0,0,0,1,0,0,1,2,1,0,0,0,1],
+            [1,1,1,0,1,0,0,0,0,0,1,1,1,0,1],
+            [1,0,0,0,1,0,1,0,1,1,1,0,1,0,1],
+            [1,0,1,0,0,0,0,0,0,1,0,0,1,0,1],
+            [1,0,1,0,1,1,0,1,0,1,1,0,1,0,1],
+            [1,0,0,0,0,0,0,1,0,0,0,0,0,0,1],
+            [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+
+        ];
+        // grupo que é responsavel na criação dos blocos de labirinto
+        this.blocks = game.add.group();
+
+        for(var row in this.maze){
+            for(var col in this.maze[row]){
+                var tile = this.maze[row][col];
+
+                //cria o x e y dos blocos
+                var x = col *50;
+                var y = row *50;
+
+                if(tile ===1){
+                    var block = this.blocks.create(x,y,'block');
+                }
+            }
+        }
     }
 
 };
